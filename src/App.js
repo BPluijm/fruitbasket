7b651ce16d43f25d71d4dce4791d867d5cb97cab
BPluijm <75165735+BPluijm@users.noreply.github.com>
2021-03-24 13:58:15 +0100

    Styling en destructering moet nog

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
 
 
 
-    const { handleSubmit, register } = useForm();
+    const { handleSubmit, register, watch, errors } = useForm();
+
+    const watchDifferent = watch("frequency");
 
 
     function reset() {
@@ -20,15 +22,18 @@ function App() {
         setKiwi(0);
     }
 
+
     function onFormSubmit(data) {
         console.log(data);
+        console.log(berry)
     }
 
     return (
     <>
+     <div id="products">
        <h1>Fruitmand bezorgservice</h1>
 
-        <fieldset>
+        <article>
             <h2>🍓 Aardbeien</h2>
             <button
                 type="button"
@@ -43,9 +48,9 @@ function App() {
             >
                 +
             </button>
-        </fieldset>
+        </article>
 
-        <fieldset>
+        <article>
             <h2>🍌 Bananen</h2>
             <button
                 type="button"
@@ -60,9 +65,9 @@ function App() {
             >
                 +
             </button>
-        </fieldset>
+        </article>
 
-        <fieldset>
+        <article>
             <h2>🍏 Appels</h2>
             <button
                 type="button"
@@ -77,9 +82,9 @@ function App() {
             >
                 +
             </button>
-        </fieldset>
+        </article>
 
-        <fieldset>
+        <article>
             <h2>🥝 Kiwi's</h2>
             <button
                 type="button"
@@ -94,7 +99,7 @@ function App() {
             >
                 +
             </button>
-        </fieldset>
+        </article>
 
         <button
             id="reset-button"
@@ -102,81 +107,137 @@ function App() {
         >
             RESET
         </button>
+     </div>
 
-
-        <div id="form">
+        <div id="formContainer">
           <form onSubmit={handleSubmit(onFormSubmit)}>
+          <div id="personal">
             <label htmlFor="firstName">
             Voornaam
             <input
                 type="text"
                 name="voornaam"
-                id="firsName"
-                ref={register}
-                />
+                id="firstName"
+                ref={register({ required: true })}
+            />
             </label>
 
             <label htmlFor="lastName">
               Achternaam
-              <input
-              type="text"
-              name="achternaam"
-              id="lastName"
-              ref={register}
+              <input type="text"
+                     name="achternaam"
+                     id="lastName"
+                     ref={register({ required: true })}
               />
             </label>
 
             <label htmlFor="age">
                Leeftijd
-               <input
-               type="number"
-               name="leeftijd"
-               id="age"
-               ref={register}
+               <input type="number"
+                      name="leeftijd"
+                      id="age"
+                      ref={register({ required: true })}
                />
             </label>
 
              <label htmlFor="postalCode">
               Postcode
-              <input
-              type="number and text"
-              name="postcode"
-              id="postalCode"
-              ref={register}
+              <input type="number and text"
+                     name="postcode"
+                     id="postalCode"
+                     ref={register({ required: true })}
               />
              </label>
 
               <label htmlFor="houseNumber">
                   Huisnummer zonder toevoeging
-                  <input
-                      type="number"
-                      name="postcode"
-                      id="houseNumber"
-                      ref={register}
+                  <input type="number"
+                         name="huisnummer"
+                         id="houseNumber"
+                         ref={register({ required: true })}
                   />
               </label>
-
-
+          </div>
+            <div id="delivery">
+                <p>Bezorgfrequentie</p>
               <label htmlFor="frequency">
-                  <input type="radio" value="weekly" name="frequency" id="weekly" ref={register}/>Iedere week
-              </label>
-              <label htmlFor="frequency">
-                  <input type="radio" value="bi-weekly" name="frequency" id="bi-weekly" ref={register}/>Om de week
-              </label>
-              <label htmlFor="frequency">
-                  <input type="radio" value="monthly" name="frequency" id="monthly" ref={register}/>Iedere maand
-              </label>
-              <label htmlFor="frequency">
-                  <input type="radio" value="different" name="frequency" id="different" ref={register}/>Anders
+                  <input type="radio"
+                         value="weekly"
+                         name="frequency"
+                         id="weekly"
+                         ref={register}
+                  />
+                  Iedere week
               </label>
 
+              <label htmlFor="frequency">
+                  <input type="radio"
+                         value="bi-weekly"
+                         name="frequency"
+                         id="bi-weekly"
+                         ref={register}
+                  />
+                  Om de week
+              </label>
+
+              <label htmlFor="frequency">
+                  <input type="radio"
+                         value="monthly"
+                         name="frequency"
+                         id="monthly"
+                         ref={register}
+                  />
+                  Iedere maand
+              </label>
+
+              <label htmlFor="frequency">
+                  <input type="radio"
+                         value="different"
+                         name="frequency"
+                         id="different"
+                         ref={register}
+                  />
+                  Anders
+              </label>
+              {watchDifferent === "different" && <textarea type="textarea" name="tekstveld" ref={register} />}
+            </div>
+
+              <label htmlFor="comments">
+                  Opmerking:
+                  <textarea
+                      name="comments"
+                      id="comments"
+                      rows="4"
+                      cols="40"
+                      ref={register(
+                          {
+                                  maxLength: {
+                                  value: 75,
+                                  message: 'Er mogen maximaal 75 karakters gebruikt worden',
+                              },
+                          }
+                      )}
+                  >
+                  </textarea>
+                  {errors.comments && <p>{errors.ref.message}</p>}
+              </label>
+
+             <div id="checkbox">
+              <input type="checkbox"
+                     id="conditions"
+                     name="conditions"
+                     ref={register ({required: true})}
+              />
+              <label htmlFor="checkbox">Ik ga akkoord met de algemene voorwaarden</label>
+                 {errors.conditions && <p id="error">Please agree with our terms and conditions</p>}
+             </div>
+
+              <button type="submit">
+                  Verzend
+              </button>
 
           </form>
         </div>
-
-
-
-
     </>
   );
 }
